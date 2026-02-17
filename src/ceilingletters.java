cde06685ba136746e5fcd7979732fa68e800022b
Mohanprasadd24 <mohanprasadd24@gmail.com>
2026-02-17 16:14:49 +0530

    letters[start%letters.length]; if no more letter is greater than the target it returns the first char

--- src/ceilingletters.java
+++ src/ceilingletters.java
@@ -3,11 +3,11 @@
 public class ceilingletters {
     public static void main(String[] args) {
         char[] arr ={'c','e','l','x','z'};
-        char target = 'x';
-        int ans= ceilletter(arr,target);
+        char target = 'z';
+        char ans= ceilletter(arr,target);
         System.out.print(ans);
     }
-    public static int ceilletter(char[] arr,char target){
+    public static char ceilletter(char[] arr,char target){
         int start = 0;
         int end= arr.length-1;
         while(start<=end){
@@ -19,6 +19,11 @@ public class ceilingletters {
                 end=mid-1;
             }
         }
-        return start;
+        if(start==arr.length){
+            return '\0';   // null character
+
+        }
+        return  arr[start];
     }
 }
+//return letters[start%letters.length]; if no more letter is greater than the target it returns the first char
